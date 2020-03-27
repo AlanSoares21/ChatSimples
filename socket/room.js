4139eae53ad57b244b5ad11dfeaab76b37c07acc
AlanSoares21 <alansoares26@outlook.com>
2020-03-27 17:08:37 -0300

    room.js -> array usersOn agora usa socket.id para colocar usuarios \n index com introdução sobre como usar

--- socket/room.js
+++ socket/room.js
@@ -20,7 +20,7 @@ module.exports = {
 
       socket.on('Nome usuario',(nomeUser)=>{
         user = { id : socket.id , name : nomeUser };
-        usersOn.push(user) ;
+        usersOn[socket.id] = user ;
         //console.log(user);
       });
 
@@ -34,15 +34,15 @@ module.exports = {
           let user = salas[sala].users.findIndex( user => user.id == socket.id);
           if(user == -1){
             //adiciona o usuario ao array salas
-            salas[sala].users.push( usersOn[ usersOn.findIndex(value => value.id == socket.id) ] )
+            salas[sala].users.push( usersOn[ socket.id ] );
           }
-
           socket.emit('usuariosOn',salas[sala]);
           socket.broadcast.to(nomeSala).emit('novo usuario',salas[sala]);
+
         }else{//sala não existe
           //criando sala
-          let user = usersOn.findIndex(user => user.id == socket.id);
-          sala = { name : nomeSala, users : [ usersOn[user] ] };
+          let user = usersOn[ socket.id ];
+          sala = { name : nomeSala, users : [ user ] };
           salas.push(sala);
           socket.emit('usuariosOn',sala);
         }
@@ -51,26 +51,23 @@ module.exports = {
       });
 
       socket.on('menssagem',(m,sala)=>{// @m => menssagem enviada; @sala => nome da sala onde vamos transmitir a menssagem
-        let user = usersOn[ usersOn.findIndex( i => i.id == socket.id) ];
+        let user = usersOn[ socket.id ];
         socket.broadcast.to(sala).emit('menssagem',m,user);
       });
 
 
       socket.on('disconnect',()=>{
-        let user = usersOn[ usersOn.findIndex( i => i.id == socket.id) ];
-
+        let user = usersOn[ socket.id ];
         salas.forEach((sala) => {
-          var indx = sala.users.findIndex( i => i == user);
-          if(indx != -1){
+          //var indx = sala.users.findIndex( i => i.id == user);
+          if(sala.users.findIndex( i => i == user) != -1){
             sala.users.splice(sala.users.indexOf(user), 1);//encontra o indice de user no array e apaga 1 registro
             io.to(sala.name).emit('usuario desconectou',sala,user);//novo formato da sala e os dados do usuario
           }
+
         });
-
-            usersOn.splice(usersOn.indexOf(user), 1);//encontra o indice de user no array e apaga 1 registro
-            console.log('usuario removido '+socket.id);
-
-
+        usersOn.splice(usersOn.indexOf(user), 1);//encontra o indice de user no array e apaga 1 registro
+        console.log('usuario removido '+socket.id);
 
       });//disconnect
 
